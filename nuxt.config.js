6a3fda6d94161cc59e21d05928f1956154af90da
Miguel Ángel <mddiosc@gmail.com>
2020-11-10 19:16:39 +0100

    fix server

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,4 +66,10 @@ export default {
     },
     rewriteRedirects: true,
   },
+
+  server: {
+    port: 443, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    timing: false,
+  },
 }
