d90f456413c21ba1a6fa246a9ab5be400243e28d
Miguel Ángel <mddiosc@gmail.com>
2020-11-10 19:40:47 +0100

    new fix 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,7 +69,7 @@ export default {
 
   server: {
     port: process.env.PORT, // default: 3000
-    host: '0.0.0.0', // default: localhost,
+    host: process.env.HOST, // default: localhost,
     timing: false,
   },
 }
